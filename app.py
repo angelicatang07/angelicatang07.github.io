9421b55ab7f08566a8d75658dff17eb11a058a1f
YTomar79 <166078547+YTomar79@users.noreply.github.com>
2024-07-23 23:58:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -109,4 +109,5 @@ def predict():
         return jsonify({'error': 'Prediction failed', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
