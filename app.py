5eb41850e2d85db67bff2ab66ab22e47a3d80904
YTomar79 <166078547+YTomar79@users.noreply.github.com>
2024-07-23 23:43:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,13 @@ max_length = 200
 padding_type = 'post'
 trunc_type = 'post'
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    return response
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
