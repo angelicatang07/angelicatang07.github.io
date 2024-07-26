cd073e0957dfcb405c39d1360151b4d7cbcfb015
YTomar79 <166078547+YTomar79@users.noreply.github.com>
2024-07-26 12:55:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 import logging
 from flask import Flask, request, jsonify
-import pickle
-import importlib
+import dill as pickle  # Use dill instead of pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import requests
 from flask_cors import CORS
-
 
 # Suppress TensorFlow logging warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -37,29 +35,23 @@ def load_model():
         logging.info(f"Model loaded successfully from {model_path}")
     return model
 
-class CustomUnpickler(pickle.Unpickler):
-    def find_class(self, module, name):
-        if module.startswith('tensorflow.keras.src'):
-            module = module.replace('tensorflow.keras.src', 'tensorflow.keras')
-        return super().find_class(module, name)
-
 def load_tokenizer():
     global tokenizer
     if tokenizer is None:
-        tokenizer_path = os.path.join(os.getcwd(), 'tokenizer.pkl')
+        tokenizer_path = os.path.join(os.getcwd(), 'tokenizer_corrected.pkl')
         download_file(tokenizer_url, tokenizer_path)
         with open(tokenizer_path, 'rb') as handle:
-            tokenizer = CustomUnpickler(handle).load()
+            tokenizer = pickle.load(handle)
         logging.info(f"Tokenizer loaded successfully from {tokenizer_path}")
     return tokenizer
 
 def load_scaler():
     global scaler
     if scaler is None:
-        scaler_path = os.path.join(os.getcwd(), 'scaler.pkl')
+        scaler_path = os.path.join(os.getcwd(), 'scaler_corrected.pkl')
         download_file(scaler_url, scaler_path)
         with open(scaler_path, 'rb') as handle:
-            scaler = CustomUnpickler(handle).load()
+            scaler = pickle.load(handle)
         logging.info(f"Scaler loaded successfully from {scaler_path}")
     return scaler
 
@@ -77,7 +69,7 @@ def download_file(url, local_path):
 # Helper function to download and extract zip files
 def download_and_extract_zip(url, extract_to):
     response = requests.get(url)
-    if response.status_code == 200:
+    if response.status_code == 200):
         zip_path = os.path.join(extract_to, 'model.zip')
         os.makedirs(extract_to, exist_ok=True)
         with open(zip_path, 'wb') as file:
