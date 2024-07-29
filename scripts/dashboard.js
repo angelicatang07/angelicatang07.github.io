eb7e0c7efdfb114d3849a5ec99ca3817b754788e
angelica-tang <angelicatang09@gmail.com>
2024-07-29 19:00:54 +0000

    reg and funny

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
             alert("Please fill in all fields and rate the book.");
             return;
         }
-        const unfinishedTaskRef = ref(database, 'unfinished_task'); // Define Firebase reference
+        const unfinishedTaskRef = ref(database, 'regular_reviews'); // Define Firebase reference
         const newTaskRef = push(unfinishedTaskRef);
     
         update(newTaskRef, {
@@ -114,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 function create_unfinished_task() {
     const unfinishedTaskContainer = document.getElementById("unfinished_tasks_container");
-    const unfinishedTaskRef = ref(database, 'unfinished_task');
+    const unfinishedTaskRef = ref(database, 'regular_reviews');
 
     onValue(unfinishedTaskRef, (snapshot) => {
         const tasks = snapshot.val();
@@ -221,7 +221,7 @@ function createTaskElement(task, key, type) {
 
 
 function task_delete(key) {
-    const taskRef = ref(database, 'unfinished_task/' + key);
+    const taskRef = ref(database, 'regular_reviews/' + key);
 
     // Remove task from database
     remove(taskRef).then(() => {
